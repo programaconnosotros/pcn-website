c0d83900a681ebd57b87b07fa2b23948ffb508ef
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-06-26 00:14:57 -0300

    feat: add some pages

---
+++ src/app/join/page.tsx
@@ -0,0 +1,8 @@
+export default function Join() {
+  return (
+    <main className="min-h-screen p-24">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Unirme</h1>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">Próximamente</p>
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,31 @@
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      programaConNosotros
-      <Button>Test</Button>
+    <main className="min-h-screen p-24">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        programaConNosotros
+      </h1>
+
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Comunidad nerd de programación e ingeniería de software en general. Sumate para compartir
+        conocimiento, experiencia y conocer más gente copada 🙌
+      </p>
+
+      <div className="mt-5 flex flex-row gap-2">
+        <Link href="/join">
+          <Button>Quiero unirme 🤚</Button>
+        </Link>
+
+        <Link href="/lightning-talks">
+          <Button variant="outline">Lightning Talks ⚡️</Button>
+        </Link>
+
+        <Link href="/advises">
+          <Button variant="outline">Consejos 👌</Button>
+        </Link>
+      </div>
     </main>
   );
 }
