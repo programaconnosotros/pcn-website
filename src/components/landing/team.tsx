b06f6e7058ffa1342c68505e6d1af571d06281b2
Alejoboga20 <alejoboga19@gmail.com>
2024-08-16 15:40:48 -0300

    refactor: create logo components

--- src/components/landing/team.tsx
+++ src/components/landing/team.tsx
@@ -1,13 +1,17 @@
-import { Presentation } from 'lucide-react';
+import { GitHubSVG } from '../logos/GitHubSVG';
+import { LinkedInSVG } from '../logos/LinkedInSVG';
+import { XLogoSVG } from '../logos/XLogoSVG';
 
-const people: Array<{
+type People = {
   name: string;
   role: string | JSX.Element;
   imageUrl: string;
   xUrl?: string;
   linkedinUrl?: string;
   githubUrl?: string;
-}> = [
+};
+
+const people: People[] = [
   {
     name: 'Agus',
     role: (
@@ -110,14 +114,7 @@ export const Team = () => (
                 <li>
                   <a href={person.xUrl} className="text-gray-400 hover:text-gray-500">
                     <span className="sr-only">X</span>
-                    <svg
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      aria-hidden="true"
-                      className="h-5 w-5"
-                    >
-                      <path d="M11.4678 8.77491L17.2961 2H15.915L10.8543 7.88256L6.81232 2H2.15039L8.26263 10.8955L2.15039 18H3.53159L8.87581 11.7878L13.1444 18H17.8063L11.4675 8.77491H11.4678ZM9.57608 10.9738L8.95678 10.0881L4.02925 3.03974H6.15068L10.1273 8.72795L10.7466 9.61374L15.9156 17.0075H13.7942L9.57608 10.9742V10.9738Z" />
-                    </svg>
+                    <XLogoSVG />
                   </a>
                 </li>
               )}
@@ -126,18 +123,7 @@ export const Team = () => (
                 <li>
                   <a href={person.linkedinUrl} className="text-gray-400 hover:text-gray-500">
                     <span className="sr-only">LinkedIn</span>
-                    <svg
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      aria-hidden="true"
-                      className="h-5 w-5"
-                    >
-                      <path
-                        d="M16.338 16.338H13.67V12.16c0-.995-.017-2.277-1.387-2.277-1.39 0-1.601 1.086-1.601 2.207v4.248H8.014v-8.59h2.559v1.174h.037c.356-.675 1.227-1.387 2.526-1.387 2.703 0 3.203 1.778 3.203 4.092v4.711zM5.005 6.575a1.548 1.548 0 11-.003-3.096 1.548 1.548 0 01.003 3.096zm-1.337 9.763H6.34v-8.59H3.667v8.59zM17.668 1H2.328C1.595 1 1 1.581 1 2.298v15.403C1 18.418 1.595 19 2.328 19h15.34c.734 0 1.332-.582 1.332-1.299V2.298C19 1.581 18.402 1 17.668 1z"
-                        clipRule="evenodd"
-                        fillRule="evenodd"
-                      />
-                    </svg>
+                    <LinkedInSVG />
                   </a>
                 </li>
               )}
@@ -146,21 +132,7 @@ export const Team = () => (
                 <li>
                   <a href={person.githubUrl} className="text-gray-400 hover:text-gray-500">
                     <span className="sr-only">GitHub</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      className="lucide lucide-github"
-                    >
-                      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                      <path d="M9 18c-4.51 2-5-2-7-2" />
-                    </svg>
+                    <GitHubSVG />
                   </a>
                 </li>
               )}
