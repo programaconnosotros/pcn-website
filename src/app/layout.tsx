e898745c8cc4c2e245b81b59783472a0f7e9e056
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-22 00:35:18 +0000

    feat: add close button for toasts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
-          <Toaster />
+          <Toaster closeButton />
         </ThemeProvider>
       </body>
     </html>
