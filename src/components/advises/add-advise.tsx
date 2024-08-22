3518e8b515674e628a5118f1ba7b06f57f7f1638
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-22 00:16:21 +0000

    feat: open dialog to create advise

--- src/components/advises/add-advise.tsx
+++ src/components/advises/add-advise.tsx
@@ -1,12 +1,35 @@
 import { PlusCircle } from 'lucide-react';
-import { Button } from '../ui/button';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Input } from '../ui/input';
 
 const AddAdvise = () => {
   return (
-    <Button className="mb-4 w-full">
-      <PlusCircle className="mr-2 h-4 w-4" />
-      Postear un consejo
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button className="mb-4 w-full">
+          <PlusCircle className="mr-2 h-4 w-4" />
+          Postear un consejo
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Postear un consejo</DialogTitle>
+        </DialogHeader>
+
+        <Input placeholder="Escribí acá tu consejo..." />
+
+        <Button>Postear</Button>
+      </DialogContent>
+    </Dialog>
   );
 };
 
