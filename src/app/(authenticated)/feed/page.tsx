a3272ca04f9d4e09d88fdda16e72456365eea45f
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-22 12:10:31 +0000

    refactor: take out code from advises to the feed page

--- src/app/(authenticated)/feed/page.tsx
+++ src/app/(authenticated)/feed/page.tsx
@@ -1,7 +1,8 @@
 import { auth } from '@/auth';
-import Advises from '@/components/advises/advises';
+import { Advises } from '@/components/advises/advises';
 import { SignOut } from '@/components/auth/sign-out';
 import { SidebarDemo } from '@/components/sidebar-demo';
+import { ThemeToggle } from '@/components/themes/theme-toggle';
 import prisma from '@/lib/prisma';
 
 const Feed = async () => {
@@ -16,8 +17,26 @@ const Feed = async () => {
 
   return (
     <div>
-      {/* <SidebarDemo /> */}
-      <Advises advises={advises} />
+      <div className="min-h-screen bg-background">
+        {/* <SidebarDemo /> */}
+
+        <nav className="border-b">
+          <div className="mx-auto flex max-w-2xl items-center justify-between px-4 py-2">
+            <h1 className="text-lg font-semibold">
+              <code>programaConNosotros</code>
+            </h1>
+
+            <div className="flex items-center gap-2">
+              <ThemeToggle />
+              <SignOut />
+            </div>
+          </div>
+        </nav>
+
+        <main className="mx-auto max-w-2xl p-4">
+          <Advises advises={advises} />
+        </main>
+      </div>
     </div>
   );
 };
--- src/components/advises/advises.tsx
+++ src/components/advises/advises.tsx
@@ -43,7 +43,7 @@ const advises = [
   },
 ];
 
-const Advises = ({
+export const Advises = ({
   advises,
 }: {
   advises: {
@@ -57,31 +57,14 @@ const Advises = ({
   }[];
 }) => {
   return (
-    <div className="min-h-screen bg-background">
-      <nav className="border-b">
-        <div className="mx-auto flex max-w-2xl items-center justify-between px-4 py-2">
-          <h1 className="text-lg font-semibold">
-            <code>programaConNosotros</code>
-          </h1>
+    <>
+      <AddAdvise />
 
-          <div className="flex items-center gap-2">
-            <ThemeToggle />
-            {/* <SignOut /> */}
-          </div>
-        </div>
-      </nav>
-
-      <main className="mx-auto max-w-2xl p-4">
-        <AddAdvise />
-
-        <div className="space-y-4">
-          {advises.map((advise) => (
-            <Advise key={advise.id} advise={advise} />
-          ))}
-        </div>
-      </main>
-    </div>
+      <div className="space-y-4">
+        {advises.map((advise) => (
+          <Advise key={advise.id} advise={advise} />
+        ))}
+      </div>
+    </>
   );
 };
-
-export default Advises;
