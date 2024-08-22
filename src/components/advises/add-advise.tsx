0aa6c68a67e24e91bdb6f3f0814770e32f4db76e
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-22 00:35:26 +0000

    feat: add form to create advise

--- src/components/advises/add-advise.tsx
+++ src/components/advises/add-advise.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import {
@@ -8,10 +10,38 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 import { Textarea } from '@/components/ui/textarea';
+import { set, z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { toast } from 'sonner';
+import { useState } from 'react';
+
+const formSchema = z.object({
+  content: z
+    .string()
+    .min(2, { message: 'El consejo debe tener entre 2 y 400 caracteres.' })
+    .max(400, { message: 'El consejo debe tener entre 2 y 400 caracteres.' }),
+});
 
 const AddAdvise = () => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      content: '',
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    form.reset();
+    toast.success('Consejo creado exitosamente!');
+    setDialogOpen(false);
+  }
+
   return (
-    <Dialog>
+    <Dialog open={dialogOpen} onOpenChange={(open) => setDialogOpen(true)}>
       <DialogTrigger asChild>
         <Button className="mb-4 w-full">
           <PlusCircle className="mr-2 h-4 w-4" />
@@ -24,9 +54,27 @@ const AddAdvise = () => {
           <DialogTitle>Postear un consejo</DialogTitle>
         </DialogHeader>
 
-        <Textarea placeholder="Escribí acá tu consejo..." />
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="content"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea placeholder="Escribí acá tu consejo..." {...field} />
+                  </FormControl>
 
-        <Button>Postear</Button>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="w-full">
+              Publicar
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
