b7cd47706143c77786041676899d5c3b74abf585
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-22 03:06:17 +0000

    chore: add prisma adapter for auth-js

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth';
 import GitHub from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import prisma from '@/lib/prisma';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 });
