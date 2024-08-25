b79c53e1151da9c83d4743fb6e6084c41555bce2
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-25 04:02:43 +0000

    feat: invoke separated sign-out action

--- src/components/auth/sign-out.tsx
+++ src/components/auth/sign-out.tsx
@@ -1,16 +1,11 @@
-import { signOut } from '@/auth';
+import { signOut } from '@/actions/auth/sign-out';
 import { Button } from '@/components/ui/button';
 import { LogOut } from 'lucide-react';
 
 export const SignOut = () => (
-  <form
-    action={async () => {
-      'use server';
-      await signOut({ redirectTo: '/' });
-    }}
-  >
+  <form action={signOut}>
     <Button className="flex gap-2" type="submit">
-      Salir <LogOut className="mr-2 h-4 w-4" />
+      Cerrar sesión <LogOut className="mr-2 h-4 w-4" />
     </Button>
   </form>
 );
