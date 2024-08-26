2e156854657fb8f3218b6fdc77d3daf6a997b155
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-26 23:09:54 +0000

    feat: add upcoming events section component

---
+++ src/components/home/upcoming-events-section.tsx
@@ -0,0 +1,33 @@
+import { Calendar } from 'lucide-react';
+import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@components/ui/card';
+
+const events = [
+  { name: 'Lightning Talks', date: 'Sábado 26 de octubre', location: 'Hotel Sheraton Tucumán' },
+];
+
+export const UpcomingEventsSection = () => (
+  <Card className="col-span-2">
+    <CardHeader>
+      <CardTitle>Próximos eventos</CardTitle>
+      <CardDescription>No te pierdas los próximos eventos de la comunidad!</CardDescription>
+    </CardHeader>
+
+    <CardContent>
+      <div className="space-y-4">
+        {events.map((event, index) => (
+          <div key={index} className="flex items-center space-x-4">
+            <Calendar className="h-4 w-4 text-muted-foreground" />
+
+            <div>
+              <p className="text-sm font-medium leading-none">{event.name}</p>
+
+              <p className="text-sm text-muted-foreground">
+                {event.date} • {event.location}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </CardContent>
+  </Card>
+);
