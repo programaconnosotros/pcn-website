a12fc2ade60e5c55e1a37ca129910f2d7152711a
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-28 22:54:36 +0000

    feat: add heading 1

---
+++ src/components/ui/heading-1.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react';
+
+export const Heading1 = ({ children }: { children: ReactNode }) => (
+  <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{children}</h1>
+);
