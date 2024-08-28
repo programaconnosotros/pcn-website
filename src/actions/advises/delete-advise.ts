404e6b79443d1f7a9604918d6ecd2bd9b33e08de
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-28 23:51:43 +0000

    feat: add server action to delete advise

---
+++ src/actions/advises/delete-advise.ts
@@ -0,0 +1,19 @@
+'use server';
+
+import { auth } from '@/auth';
+import prisma from '@/lib/prisma';
+import { revalidatePath } from 'next/cache';
+
+export const deleteAdvise = async (id: string) => {
+  const session = await auth();
+
+  if (!session) throw new Error('User not authenticated');
+  if (!session.user?.id) throw new Error('User id not found in session');
+
+  await prisma.advise.delete({
+    where: { id },
+  });
+
+  revalidatePath('/advises');
+  revalidatePath('/home');
+};
