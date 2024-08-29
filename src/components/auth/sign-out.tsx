9453a33869448b1688f07c7be631cb7ed297ac26
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-29 00:38:04 +0000

    feat: use outline variant for sign out button

--- src/components/auth/sign-out.tsx
+++ src/components/auth/sign-out.tsx
@@ -3,8 +3,8 @@ import { Button } from '@/components/ui/button';
 import { LogOut } from 'lucide-react';
 
 export const SignOut = () => (
-  <form action={signOut}>
-    <Button className="flex gap-2" type="submit">
+  <form action={signOut} className="mt-4">
+    <Button className="flex w-full gap-2 sm:w-auto" type="submit" variant="outline">
       Cerrar sesión <LogOut className="mr-2 h-4 w-4" />
     </Button>
   </form>
