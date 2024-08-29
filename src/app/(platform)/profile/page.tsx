65f78bf76ecb193b709c763e05b4436a297674e8
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-29 00:25:02 +0000

    feat: use profile form

--- src/app/(platform)/profile/page.tsx
+++ src/app/(platform)/profile/page.tsx
@@ -1,10 +1,29 @@
+import { Heading2 } from '@components/ui/heading-2';
 import { SignOut } from '@components/auth/sign-out';
+import { ProfileForm } from '@components/profile/profile-form';
+import { auth } from '@/auth';
+import prisma from '@/lib/prisma';
 
-const Profile = () => (
-  <div className="mb-4 flex justify-between">
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Mi perfil</h1>
-    <SignOut />
-  </div>
-);
+const Profile = async () => {
+  const session = await auth();
+
+  if (!session) throw new Error('User not found');
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session.user?.id,
+    },
+  });
+
+  if (!user) throw new Error('User not found');
+
+  return (
+    <div className="mt-4 md:px-20">
+      <Heading2>Mi perfil</Heading2>
+      <ProfileForm user={user} />
+      <SignOut />
+    </div>
+  );
+};
 
 export default Profile;
