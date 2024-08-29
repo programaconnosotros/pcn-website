318cbd0ae7e77f3a1b96f964af94a862f9597003
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-29 01:22:02 +0000

    feat: change icons for sidebar items

--- src/components/ui/sidebar-container.tsx
+++ src/components/ui/sidebar-container.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { ReactNode, useState } from 'react';
 import { Sidebar, SidebarBody, SidebarLink } from './sidebar';
-import { IconBrandTabler, IconUserBolt } from '@tabler/icons-react';
+import { IconUserBolt, IconHome, IconHandGrab } from '@tabler/icons-react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
@@ -14,15 +14,13 @@ export const SidebarContainer = ({ user, children }: { user?: User; children: Re
     {
       label: 'Inicio',
       href: '/home',
-      icon: (
-        <IconBrandTabler className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
-      ),
+      icon: <IconHome className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />,
     },
     {
       label: 'Consejos',
       href: '/advises',
       icon: (
-        <IconBrandTabler className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
+        <IconHandGrab className="h-5 w-5 flex-shrink-0 text-neutral-700 dark:text-neutral-200" />
       ),
     },
   ];
