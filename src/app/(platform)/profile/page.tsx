e48854aa5d531331bb70b41965977a3d55b4f338
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-29 01:36:55 +0000

    feat: show user image with the heading

--- src/app/(platform)/profile/page.tsx
+++ src/app/(platform)/profile/page.tsx
@@ -3,6 +3,7 @@ import { SignOut } from '@components/auth/sign-out';
 import { ProfileForm } from '@components/profile/profile-form';
 import { auth } from '@/auth';
 import prisma from '@/lib/prisma';
+import Image from 'next/image';
 
 const Profile = async () => {
   const session = await auth();
@@ -19,7 +20,20 @@ const Profile = async () => {
 
   return (
     <div className="mt-4 px-6 md:px-20">
-      <Heading2>Mi perfil</Heading2>
+      <div className="mb-4 flex items-center gap-4">
+        {user.image && (
+          <Image
+            src={user.image}
+            alt="Profile picture"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+        )}
+
+        <Heading2>Mi perfil</Heading2>
+      </div>
+
       <ProfileForm user={user} />
       <SignOut />
     </div>
