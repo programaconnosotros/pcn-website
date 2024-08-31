d12c92c2502d63498fc9202a75836354fef73597
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-31 04:04:57 +0000

    feat: don't show the add advise button for anonymous users

--- src/app/(platform)/advises/page.tsx
+++ src/app/(platform)/advises/page.tsx
@@ -2,11 +2,14 @@ import { AddAdvise } from '@components/advises/add-advise';
 import { AdviseCard } from '@components/advises/advise-card';
 import prisma from '@/lib/prisma';
 import { Heading2 } from '@components/ui/heading-2';
+import { auth } from '@/auth';
 
 // TODO: Add infinite scroll to load more advises and improve performance.
 // TODO: Allow users to order advises by number of likes.
 
 const AdvisesPage = async () => {
+  const session = await auth();
+
   const advises = await prisma.advise.findMany({
     include: {
       author: true,
@@ -20,7 +23,7 @@ const AdvisesPage = async () => {
     <div className="mt-4 md:px-20">
       <div className="mb-6 flex items-center justify-between">
         <Heading2>Consejos</Heading2>
-        <AddAdvise />
+        {session && <AddAdvise />}
       </div>
 
       <div className="col-span-1 mb-10 space-y-4 md:grid md:grid-cols-2 md:gap-4 md:space-y-0 lg:grid-cols-3 lg:gap-6">
