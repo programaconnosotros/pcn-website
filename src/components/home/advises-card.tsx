e1d6026d8d290eac1699554c5d1da0a15c98bf03
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-31 04:10:09 +0000

    feat: redirect to advises page in advise card of the home

--- src/components/home/advises-card.tsx
+++ src/components/home/advises-card.tsx
@@ -1,21 +1,26 @@
 import { MessageSquare } from 'lucide-react';
 import { Card, CardHeader, CardTitle, CardContent } from '@components/ui/card';
 import prisma from '@/lib/prisma';
+import Link from 'next/link';
 
 export const AdvisesCard = async () => {
   const numberOfAdvises = await prisma.advise.count({});
 
   return (
-    <Card>
-      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">Consejos</CardTitle>
-        <MessageSquare className="h-4 w-4 text-muted-foreground" />
-      </CardHeader>
+    <Link href="/advises" className="block">
+      <Card className="cursor-pointer transition-shadow hover:shadow-md">
+        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+          <CardTitle className="text-sm font-medium">Consejos</CardTitle>
+          <MessageSquare className="h-4 w-4 text-muted-foreground" />
+        </CardHeader>
 
-      <CardContent>
-        <div className="text-2xl font-bold">{numberOfAdvises}</div>
-        <p className="text-xs text-muted-foreground">A seguir compartiendo conocimiento valioso!</p>
-      </CardContent>
-    </Card>
+        <CardContent>
+          <div className="text-2xl font-bold">{numberOfAdvises}</div>
+          <p className="text-xs text-muted-foreground">
+            A seguir compartiendo conocimiento valioso!
+          </p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
