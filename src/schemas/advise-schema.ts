9bbb7b3ca349f135c02a58702a9c0895c90c5951
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-08-31 03:55:18 +0000

    feat: set 1000 as max size for content

--- src/schemas/advise-schema.ts
+++ src/schemas/advise-schema.ts
@@ -4,7 +4,7 @@ export const adviseSchema = z.object({
   content: z
     .string()
     .min(10, { message: 'Tenés que escribir al menos 10 caracteres' })
-    .max(400, { message: 'Podés escribir 400 caracteres como máximo' }),
+    .max(1000, { message: 'Podés escribir 1000 caracteres como máximo' }),
 });
 
 export type AdviseFormData = z.infer<typeof adviseSchema>;
