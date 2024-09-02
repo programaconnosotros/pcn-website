b42ad490174c9914cd0a984bcfafd23f8c3c808e
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-02 22:08:48 +0000

    feat: add background overlay card

---
+++ src/components/ui/background-overlay-card.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { cn } from '@/lib/utils';
+
+export const BackgroundOverlayCard = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => (
+  <div className="w-full">
+    <div
+      className={cn(
+        'card group relative flex h-96 w-full cursor-pointer flex-col justify-end overflow-hidden rounded-md border border-transparent p-4 shadow-xl dark:border-neutral-800',
+        'bg-opacity-50 bg-[url(http://localhost:3000/starbucks.jpeg)] bg-cover',
+        // Preload hover image by setting it in a pseudo-element
+        'before:fixed before:inset-0 before:z-[-1] before:bg-[url(http://localhost:3000/starbucks.jpeg)] before:opacity-0',
+        'hover:bg-[url(http://localhost:3000/starbucks.jpeg)]',
+        "after:absolute after:inset-0 after:bg-black after:opacity-50 after:content-['']",
+        'transition-all duration-500',
+      )}
+    >
+      <div className="text relative z-50">
+        <h1 className="text-md relative font-bold text-gray-50 md:text-xl">{title}</h1>
+        <p className="relative my-4 text-base font-normal text-gray-50">{description}</p>
+      </div>
+    </div>
+  </div>
+);
