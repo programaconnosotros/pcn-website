ee2ed46c7245c563f4b6a7117f699d02c3e89749
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-03 20:49:36 +0000

    chore: accept images from aceternity and unsplash

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com'],
+    domains: ['avatars.githubusercontent.com', 'assets.aceternity.com', 'images.unsplash.com'],
   },
 };
 
