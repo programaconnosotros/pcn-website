b6039f9cf6496e71c61007e089fc9ec5fa6c0ec1
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-04 02:11:28 +0000

    refactor: use remotePatterns instead of domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'assets.aceternity.com', 'images.unsplash.com'],
+    remotePatterns: [
+      {
+        hostname: 'avatars.githubusercontent.com',
+        protocol: 'https',
+      },
+      {
+        hostname: 'assets.aceternity.com',
+        protocol: 'https',
+      },
+      {
+        hostname: 'images.unsplash.com',
+        protocol: 'https',
+      },
+    ],
   },
 };
 
