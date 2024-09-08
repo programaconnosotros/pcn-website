4e67e47ce35dea7fac2ef89acfe46a36c6b86e1b
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-08 20:38:22 +0000

    feat: allow to receive className

--- src/components/ui/heading-2.tsx
+++ src/components/ui/heading-2.tsx
@@ -1,7 +1,10 @@
-import { ReactNode } from "react";
+import { cn } from '@/lib/utils';
+import { ReactNode } from 'react';
 
-export const Heading2 = ({ children }: { children: ReactNode }) => ( 
-  <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+export const Heading2 = ({ children, className }: { children: ReactNode; className?: string }) => (
+  <h2
+    className={cn('scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0', className)}
+  >
     {children}
   </h2>
-)
+);
--- src/components/ui/heading-3.tsx
+++ src/components/ui/heading-3.tsx
@@ -1,3 +1,11 @@
-export const Heading3 = ({ children }: { children: React.ReactNode }) => (
-  <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>
+import { cn } from '@/lib/utils';
+
+export const Heading3 = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => (
+  <h3 className={cn('scroll-m-20 text-2xl font-semibold tracking-tight', className)}>{children}</h3>
 );
