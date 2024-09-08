157aae2cd4a20a15769ac2b33a875bb1d64cb51f
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-08 22:53:45 +0000

    feat: add latex course page

---
+++ src/app/(platform)/courses/latex/page.tsx
@@ -0,0 +1,45 @@
+import { Card } from '@/components/ui/card';
+import { Heading2 } from '@/components/ui/heading-2';
+
+const LaTeXCourse = () => (
+  <div className="mt-4 md:px-20">
+    <Heading2>LaTeX</Heading2>
+
+    <div className="mt-4 w-full" style={{ aspectRatio: '16/9' }}>
+      <Card className="mb-8 h-full w-full p-8">
+        <iframe
+          className="h-full w-full"
+          src="https://www.youtube.com/embed/mYlqUGYp0_U?si=50HztuG6GLT1MI1J"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </Card>
+
+      <Card className="mb-8 h-full w-full p-8">
+        <iframe
+          className="h-full w-full"
+          src="https://www.youtube.com/embed/OZtjjLzpyWE?si=fEafCnewwKCIjFEZ"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </Card>
+
+      <Card className="mb-8 h-full w-full p-8">
+        <iframe
+          className="h-full w-full"
+          src="https://www.youtube.com/embed/ZzGJGiY0v70?si=ITMznfAjCBdRL2QT"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </Card>
+    </div>
+  </div>
+);
+
+export default LaTeXCourse;
