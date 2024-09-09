85339dce978d0c8dd54b81580956d18c22563cfd
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-09 04:08:59 +0000

    feat: add code warfare page

---
+++ src/app/(platform)/code-warfare/page.tsx
@@ -0,0 +1,10 @@
+import { Heading2 } from '@/components/ui/heading-2';
+
+const CodeWarfare = () => (
+  <div className="mt-4 md:px-20">
+    <Heading2>Code Warfare</Heading2>
+    <div className="mt-4">Próximamente.</div>
+  </div>
+);
+
+export default CodeWarfare;
