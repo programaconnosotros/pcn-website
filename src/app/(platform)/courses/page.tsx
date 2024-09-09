978d874f2539503669d850cff2c68b5323e95e23
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-09 03:34:42 +0000

    refactor: extract view button and add icon

--- src/app/(platform)/courses/page.tsx
+++ src/app/(platform)/courses/page.tsx
@@ -2,99 +2,102 @@ import { Heading2 } from '@/components/ui/heading-2';
 import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { Send } from 'lucide-react';
+import { Send, TvMinimalPlay } from 'lucide-react';
 
-const Courses = () => (
-  <div className="mt-4 md:px-20">
-    <Heading2>Cursos</Heading2>
+const Courses = () => {
+  const ViewButton = ({ href }: { href: string }) => (
+    <Link href={href}>
+      <Button className="flex flex-row items-center gap-2">
+        Ver curso
+        <TvMinimalPlay className="h-5 w-5" />
+      </Button>
+    </Link>
+  );
 
-    <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2">
-      <Card className="flex flex-col justify-between">
-        <CardHeader>
-          <CardTitle>Git & GitHub</CardTitle>
+  return (
+    <div className="mt-4 md:px-20">
+      <Heading2>Cursos</Heading2>
 
-          <CardDescription>
-            Git permite que puedas controlar las versiones del código que desarrollas, y GitHub hace
-            posible que puedas trabajar en equipo de una manera más eficiente. Este curso fue
-            diseñado y dictado a los alumnos del primer año de ingeniería en sistemas de
-            información, de la UTN-FRT.
-          </CardDescription>
-        </CardHeader>
+      <div className="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2">
+        <Card className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle>Git & GitHub</CardTitle>
 
-        <CardFooter className="flex flex-row justify-between gap-4">
-          <p className="text-xs text-muted-foreground">
-            Curso dictado por Agustín Sánchez, Marcelo Núñez, Germán Navarro e Iván Taddei.
-          </p>
+            <CardDescription>
+              Git permite que puedas controlar las versiones del código que desarrollas, y GitHub
+              hace posible que puedas trabajar en equipo de una manera más eficiente. Este curso fue
+              diseñado y dictado a los alumnos del primer año de ingeniería en sistemas de
+              información, de la UTN-FRT.
+            </CardDescription>
+          </CardHeader>
 
-          <Link href="/courses/git-and-github">
-            <Button>Ver curso</Button>
-          </Link>
-        </CardFooter>
-      </Card>
+          <CardFooter className="flex flex-row justify-between gap-4">
+            <ViewButton href="/courses/git-and-github" />
 
-      <Card className="flex flex-col justify-between">
-        <CardHeader>
-          <CardTitle>Vim</CardTitle>
+            <p className="text-xs text-muted-foreground">
+              Curso dictado por Agustín Sánchez, Marcelo Núñez, Germán Navarro e Iván Taddei.
+            </p>
+          </CardFooter>
+        </Card>
 
-          <CardDescription>
-            Aprendé a usar Vim, el editor de texto más poderoso del mundo. Vim permite editar y
-            navegar código de una manera súper veloz. Podés usar Vim en la consola o instalar un
-            plugin dentro de tu IDE favorito para agilizar tu codificación usando los atajos de
-            teclado de Vim.
-          </CardDescription>
-        </CardHeader>
+        <Card className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle>Vim</CardTitle>
 
-        <CardFooter className="flex flex-row justify-between gap-4">
-          <p className="text-sm text-muted-foreground">Curso dictado por Esteban Sánchez.</p>
+            <CardDescription>
+              Aprendé a usar Vim, el editor de texto más poderoso del mundo. Vim permite editar y
+              navegar código de una manera súper veloz. Podés usar Vim en la consola o instalar un
+              plugin dentro de tu IDE favorito para agilizar tu codificación usando los atajos de
+              teclado de Vim.
+            </CardDescription>
+          </CardHeader>
 
-          <Link href="/courses/vim">
-            <Button>Ver curso</Button>
-          </Link>
-        </CardFooter>
-      </Card>
+          <CardFooter className="flex flex-row justify-between gap-4">
+            <ViewButton href="/courses/vim" />
+            <p className="text-sm text-muted-foreground">Curso dictado por Esteban Sánchez.</p>
+          </CardFooter>
+        </Card>
 
-      <Card className="flex flex-col justify-between">
-        <CardHeader>
-          <CardTitle>LaTeX</CardTitle>
+        <Card className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle>LaTeX</CardTitle>
 
-          <CardDescription>
-            Aprende a usar LaTeX, el sistema de composición de textos más utilizado en la academia.
-            LaTeX permite crear documentos que se ven profesionales y que se diferencian de los
-            documentos de texto típicos, además de que podés editarlos utilizando código,
-            permitiendo que uses tu tiempo en lo que importa: escribir tu documento.
-          </CardDescription>
-        </CardHeader>
+            <CardDescription>
+              Aprende a usar LaTeX, el sistema de composición de textos más utilizado en la
+              academia. LaTeX permite crear documentos que se ven profesionales y que se diferencian
+              de los documentos de texto típicos, además de que podés editarlos utilizando código,
+              permitiendo que uses tu tiempo en lo que importa: escribir tu documento.
+            </CardDescription>
+          </CardHeader>
 
-        <CardFooter className="flex flex-row justify-between gap-4">
-          <p className="text-sm text-muted-foreground">Curso dictado por Esteban Sánchez.</p>
+          <CardFooter className="flex flex-row justify-between gap-4">
+            <ViewButton href="/courses/latex" />
+            <p className="text-sm text-muted-foreground">Curso dictado por Esteban Sánchez.</p>
+          </CardFooter>
+        </Card>
 
-          <Link href="/courses/latex">
-            <Button>Ver curso</Button>
-          </Link>
-        </CardFooter>
-      </Card>
+        <Card className="flex flex-col justify-between">
+          <CardHeader>
+            <CardTitle>¿Quéres sumar tu curso aquí?</CardTitle>
 
-      <Card className="flex flex-col justify-between">
-        <CardHeader>
-          <CardTitle>¿Quéres sumar tu curso aquí?</CardTitle>
+            <CardDescription>
+              Si querés sumar tu curso a esta página para que puedan verlo todos los miembros de la
+              comunidad, contactanos clickeando el botón de abajo!
+            </CardDescription>
+          </CardHeader>
 
-          <CardDescription>
-            Si querés sumar tu curso a esta página para que puedan verlo todos los miembros de la
-            comunidad, contactanos clickeando el botón de abajo!
-          </CardDescription>
-        </CardHeader>
-
-        <CardFooter className="flex flex-row justify-between gap-4">
-          <Link className="block" href="https://wa.me/5493815777562">
-            <Button className="mt-4 flex flex-row items-center gap-2">
-              Contactar
-              <Send className="h-5 w-5" />
-            </Button>
-          </Link>
-        </CardFooter>
-      </Card>
+          <CardFooter className="flex flex-row justify-between gap-4">
+            <Link className="block" href="https://wa.me/5493815777562">
+              <Button className="mt-4 flex flex-row items-center gap-2">
+                Contactar
+                <Send className="h-5 w-5" />
+              </Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Courses;
