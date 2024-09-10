681ca76ae82331d272c7c30f04905e2f576b76ec
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-10 04:38:17 +0000

    feat: add text to scroll down

--- src/components/landing/scroll-arrow.tsx
+++ src/components/landing/scroll-arrow.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { ChevronDown } from 'lucide-react';
 
 export const ScrollArrow: React.FC = () => (
-  <div className="absolute bottom-8 transform animate-bounce">
+  <div className="absolute bottom-8 flex transform animate-bounce flex-col items-center gap-2">
+    <p className="text-xl">Scrolleá para ver más</p>
     <ChevronDown className="h-8 w-8 text-white" />
   </div>
 );
