8fc8f22a5139f9659892e61393d290cd5240de6f
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-10 04:38:34 +0000

    feat: remove unnecessary type

--- src/components/landing/scroll-arrow.tsx
+++ src/components/landing/scroll-arrow.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { ChevronDown } from 'lucide-react';
 
-export const ScrollArrow: React.FC = () => (
+export const ScrollArrow = () => (
   <div className="absolute bottom-8 flex transform animate-bounce flex-col items-center gap-2">
     <p className="text-xl">Scrolleá para ver más</p>
     <ChevronDown className="h-8 w-8 text-white" />
