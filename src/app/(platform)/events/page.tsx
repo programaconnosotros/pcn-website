23461fd3a4be9a1ad62a5e3e7adc324303ed7d33
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-10 05:17:30 +0000

    feat: create events page

---
+++ src/app/(platform)/events/page.tsx
@@ -0,0 +1,10 @@
+import { Heading2 } from '@/components/ui/heading-2';
+
+const Events = () => (
+  <div className="mt-4 md:px-20">
+    <Heading2>Eventos</Heading2>
+    <div className="mt-4">Próximamente.</div>
+  </div>
+);
+
+export default Events;
