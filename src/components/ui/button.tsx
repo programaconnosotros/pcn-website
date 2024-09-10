9dea26989676cdc5e04643b58117e76777ea913a
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-10 04:56:16 +0000

    feat: add youtube variant

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -15,6 +15,7 @@ const buttonVariants = cva(
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
+        youtube: 'bg-red-600 text-white hover:bg-red-600/90',
       },
       size: {
         default: 'h-10 px-4 py-2',
