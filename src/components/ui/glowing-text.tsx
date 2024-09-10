67d212da145f1ec3d692561e9db8e6d505cb0083
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-10 04:12:28 +0000

    fix: text size in small texts

--- src/components/ui/glowing-text.tsx
+++ src/components/ui/glowing-text.tsx
@@ -1,10 +1,10 @@
 export const GlowingText = ({ children }: { children: React.ReactNode }) => (
   <>
-    <span className="absolute mx-auto box-content flex w-fit select-none border bg-gradient-to-r from-pcnGreen via-teal-500 to-pcnGreen bg-clip-text py-4 text-center text-6xl font-extrabold text-transparent blur-xl">
+    <span className="absolute mx-auto box-content flex w-fit select-none border bg-gradient-to-r from-pcnGreen via-teal-500 to-pcnGreen bg-clip-text py-4 text-center text-3xl font-extrabold text-transparent blur-xl md:text-5xl lg:text-6xl">
       {children}
     </span>
 
-    <h1 className="relative top-0 flex h-auto w-fit select-auto items-center justify-center bg-gradient-to-r from-pcnGreen via-teal-500 to-pcnGreen bg-clip-text py-4 text-center text-6xl font-extrabold text-transparent">
+    <h1 className="relative top-0 flex h-auto w-fit select-auto items-center justify-center bg-gradient-to-r from-pcnGreen via-teal-500 to-pcnGreen bg-clip-text py-4 text-center text-3xl font-extrabold text-transparent md:text-5xl lg:text-6xl">
       <code>{children}</code>
     </h1>
   </>
