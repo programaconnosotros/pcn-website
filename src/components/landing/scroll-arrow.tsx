c4a13c1b6d5d94d4d2e88b8972ef7c5589d5ad14
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-12 21:31:53 +0000

    feat: set text-white for text

--- src/components/landing/scroll-arrow.tsx
+++ src/components/landing/scroll-arrow.tsx
@@ -2,7 +2,7 @@ import { ChevronDown } from 'lucide-react';
 
 export const ScrollArrow = () => (
   <div className="absolute bottom-8 flex transform animate-bounce flex-col items-center gap-2">
-    <p className="text-xl">Scrolleá para ver más</p>
+    <p className="text-xl text-white">Scrolleá para ver más</p>
     <ChevronDown className="h-8 w-8 text-white" />
   </div>
 );
