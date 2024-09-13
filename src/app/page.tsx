4d5803377f1216053a289244ec5c607800e024bc
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-13 19:42:38 +0000

    refactor: extract footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import { CommunityHighlights } from '@/components/landing/community-highlights';
 import { Discord } from '@/components/landing/discord';
 import { PlatformFeatures } from '@/components/landing/platform-features';
 import { Hero } from '@/components/landing/hero';
-import { ThemeToggle } from '@/components/themes/theme-toggle';
-import { Github } from 'lucide-react';
-import Link from 'next/link';
+import { Footer } from '@/components/landing/footer';
 
 // TODO: Add testimonials.
 // TODO: Add FAQ.
@@ -26,31 +24,9 @@ export const Home = async () => {
         <Activities />
         <PlatformFeatures />
         <Discord />
-
-        {/* TODO: Add section to show dynamic data (number of users, advises, activities, etc). */}
       </main>
 
-      <footer className="flex w-full shrink-0 flex-row items-center justify-between gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
-        <div className="flex items-center gap-4">
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            <code>programaConNosotros</code>
-          </p>
-
-          <div className="flex space-x-4">
-            <Link
-              href="https://github.com/programaconnosotros/pcn-website"
-              className="text-gray-500 hover:text-gray-700"
-            >
-              <Github className="h-6 w-6" />
-              <span className="sr-only">GitHub</span>
-            </Link>
-          </div>
-        </div>
-
-        <div className="flex justify-end">
-          <ThemeToggle />
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
