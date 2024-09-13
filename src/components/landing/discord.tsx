bbe8208c3cc943ab6630b830a7d7ac6c01147914
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-13 19:46:06 +0000

    feat: improve vertical spacing

--- src/components/landing/discord.tsx
+++ src/components/landing/discord.tsx
@@ -5,26 +5,24 @@ export const Discord = () => (
   <section className="w-full py-12 dark:bg-black md:py-24 lg:py-32">
     <div className="container px-4 md:px-6">
       <div className="flex flex-col items-center space-y-4 text-center">
-        <div className="space-y-2">
-          <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Sumate en Discord!</h2>
+        <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Sumate en Discord!</h2>
 
-          <img alt="Discord" height="310" src="/discord-demo.png" />
+        <img alt="Discord" height="310" src="/discord-demo.png" />
 
-          <p className="mx-auto max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-            Tenemos canales de texto y voz para que puedas interactuar con los miembros de la
-            comunidad, y un foro súper copado donde podes encontrar o compartir información valiosa!
-          </p>
+        <p className="mx-auto max-w-[800px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+          Tenemos canales de texto y voz para que puedas interactuar con los miembros de la
+          comunidad, y un foro súper copado donde podes encontrar o compartir información valiosa!
+        </p>
 
-          <p className="mx-auto max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-            Ya seas un desarrollador súper experimentado, o una persona que recién está empezando a
-            explorar el universo de la programación, todos son bienvenidos en nuestra comunidad.
-            Animate!
-          </p>
+        <p className="mx-auto max-w-[800px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+          Ya seas un desarrollador súper experimentado, o una persona que recién está empezando a
+          explorar el universo de la programación, todos son bienvenidos en nuestra comunidad.
+          Animate!
+        </p>
 
-          <Link href="https://discord.gg/dTQexKw56S" target="_blank">
-            <Button className="mt-8">Ir a Discord</Button>
-          </Link>
-        </div>
+        <Link href="https://discord.gg/dTQexKw56S" target="_blank">
+          <Button className="mt-8">Ir a Discord</Button>
+        </Link>
       </div>
     </div>
   </section>
