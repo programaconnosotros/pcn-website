f4a2f35261656e85a9f6ea013ab1dfd963732f4e
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-14 04:18:34 +0000

    feat: add new talk and fix date format

--- src/app/(platform)/talks/page.tsx
+++ src/app/(platform)/talks/page.tsx
@@ -361,12 +361,18 @@ export const talks: Talk[] = [
     date: new Date('2020-07-04'),
     youtubeUrl: 'https://www.youtube.com/live/L4RP34KjHns?si=NRFGpSTE7D0VWJvw&t=9400',
   },
+  {
+    name: 'Una mirada a la industria del software',
+    date: new Date('2023-11-24'),
+    speakerName: 'Agustín Sánchez & Marcelo Nuñez',
+    speakerPhoto: '/una-mirada-a-la-industria-del-software.jpeg',
+  },
 ];
 
 const Talks = () => (
   <div className="mt-4 md:px-20">
     <div className="mb-8 flex w-full flex-row items-center justify-between">
-      <Heading2>Lightning Talks</Heading2>
+      <Heading2>Charlas</Heading2>
 
       <Link className="block" href="https://wa.me/5493815777562">
         <Button className="flex flex-row items-center gap-2">
@@ -396,7 +402,13 @@ const Talks = () => (
 
             <CardContent>
               <p className="text-sm text-muted-foreground">
-                {new Date(talk.date).toLocaleDateString()}
+                {new Date(talk.date)
+                  .toLocaleDateString('es-ES', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                  })
+                  .replace(/\//g, '/')}
               </p>
             </CardContent>
 
