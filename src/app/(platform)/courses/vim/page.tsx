84281308ef6b3cf2d0751adbf937e2bd4f94a4ef
José Agustín Sánchez <55034666+agustin-sanc@users.noreply.github.com>
2024-09-14 05:42:41 +0000

    refactor: create classes array and add carrousel

--- src/app/(platform)/courses/vim/page.tsx
+++ src/app/(platform)/courses/vim/page.tsx
@@ -1,4 +1,3 @@
-import { Card } from '@/components/ui/card';
 import { Heading2 } from '@/components/ui/heading-2';
 import {
   Breadcrumb,
@@ -8,8 +7,22 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 // TODO: Agregar botón para donar al profesor del curso.
+
+const classes = [
+  'https://www.youtube.com/embed/C-C4xoCj_Lw?si=ik0XqUGkz4Xw7hZ4',
+  'https://www.youtube.com/embed/0s1ccWvLGYw?si=ph1rPlNvBoobmbG8',
+  'https://www.youtube.com/embed/9VIma5G-gUc?si=dHyZoKRqvc3Vpi0S',
+  'https://www.youtube.com/embed/WVzklaR68PM?si=1b4QIepxrw44Tw45',
+];
 
 const VimCourse = () => (
   <div className="mt-4 md:px-20">
@@ -35,50 +48,28 @@ const VimCourse = () => (
 
     <Heading2>Vim</Heading2>
 
-    <div className="mt-4 w-full" style={{ aspectRatio: '16/9' }}>
-      <Card className="mb-8 h-full w-full p-8">
-        <iframe
-          className="h-full w-full"
-          src="https://www.youtube.com/embed/C-C4xoCj_Lw?si=ik0XqUGkz4Xw7hZ4"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-      </Card>
+    <div className="mt-12 pr-20">
+      <Carousel>
+        <CarouselContent>
+          {classes.map((classUrl) => (
+            <CarouselItem key={classUrl}>
+              <div className="w-full" style={{ aspectRatio: '16/9' }}>
+                <iframe
+                  className="h-full w-full"
+                  src={classUrl}
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  referrerPolicy="strict-origin-when-cross-origin"
+                  allowFullScreen
+                />
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
 
-      <Card className="mb-8 h-full w-full p-8">
-        <iframe
-          className="h-full w-full"
-          src="https://www.youtube.com/embed/0s1ccWvLGYw?si=ph1rPlNvBoobmbG8"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-      </Card>
-
-      <Card className="mb-8 h-full w-full p-8">
-        <iframe
-          className="h-full w-full"
-          src="https://www.youtube.com/embed/9VIma5G-gUc?si=dHyZoKRqvc3Vpi0S"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-      </Card>
-
-      <Card className="mb-8 h-full w-full p-8">
-        <iframe
-          className="h-full w-full"
-          src="https://www.youtube.com/embed/WVzklaR68PM?si=1b4QIepxrw44Tw45"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        />
-      </Card>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   </div>
 );
