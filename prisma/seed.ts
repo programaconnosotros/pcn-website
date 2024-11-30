08f6bb0c0addaf73aa051d87bbee046e09a8ca0f
MauriJC <xaviermauricio33@gmail.com>
2024-11-30 22:57:46 +0000

    feat(seeder): Added seeders for Images and Events

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -35,6 +35,50 @@ async function main() {
     ),
   );
 
+
+  // Create at least 10 mocked events
+  const events = await Promise.all(
+    Array.from({ length: 10 }).map(async (_, index) => {
+      const startDate = new Date();
+      const endDate = new Date(startDate);
+      endDate.setHours(startDate.getHours() + 4);
+
+      // Generate only for even events
+      const includeImages = index % 2 === 0;
+      const imageIds: string[] = [];
+
+      if (includeImages) {
+        const images = await Promise.all(
+          Array.from({ length: 3 }).map(async (_, i) => {
+            const image = await prisma.image.create({
+              data: {
+                imgSrc: `/events/Lightning talks flyer.jpg`, // 
+              },
+            });
+            imageIds.push(image.id);
+            return image;
+          })
+        );
+      }
+
+      const event = await prisma.event.create({
+        data: {
+          name: `Titulo del evento numero ${index + 1}`,
+          flyerSrc: "/events/Lightning talks flyer.jpg",
+          description: `Esta es la descripción del contenido del evento numero ${index + 1}`,
+          date: startDate,
+          endDate: endDate,
+          location: "San Miguel de Tucumán",
+          images: {
+            connect: imageIds.length > 0 ? imageIds.map(id => ({ id })) : [],
+          },
+        },
+      });
+
+      return event;
+    }),
+  );
+
   console.log(`Seed data created successfully!`);
 }
 
