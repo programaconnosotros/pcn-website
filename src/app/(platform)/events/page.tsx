20d23454b5838969eed524758e3bbf328caad5fb
MauriJC <xaviermauricio33@gmail.com>
2025-01-09 19:20:56 +0000

    chore(EventsPage): renamed component Events to EventsPage to keep consistance

--- src/app/(platform)/events/page.tsx
+++ src/app/(platform)/events/page.tsx
@@ -1,11 +1,11 @@
 import { EventsList } from '@/components/events/events-list';
 import { Heading2 } from '@/components/ui/heading-2';
 
-const Events = () => (
+const EventsPage = () => (
   <div className="mt-4 md:px-20">
     <Heading2>Eventos</Heading2>
     <EventsList></EventsList>
   </div>
 );
 
-export default Events;
+export default EventsPage;
