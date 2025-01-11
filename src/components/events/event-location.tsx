bac66924b6373463a70ca3b684d773ab0ac5c227
MauriJC <xaviermauricio33@gmail.com>
2025-01-11 20:51:08 +0000

    feat(event-location): addeed new UI to include the new fields (city, address and placeName)

--- src/components/events/event-location.tsx
+++ src/components/events/event-location.tsx
@@ -4,31 +4,44 @@ import { Heading3 } from '../ui/heading-3'
 type EventLocationProps = {
     latitude?: number | null;
     longitude?: number | null;
+    city: string;
+    address: String;
+    placeName: String;
 }
 
-const EventLocation: React.FC<EventLocationProps> = ({ latitude, longitude }) => {
+const EventLocation: React.FC<EventLocationProps> = ({ latitude, longitude, city, address, placeName }) => {
     return (
         <div>
             <Heading3 className="text-2xl font-semibold mt-4 mb-4">
                 Ubicación del evento
             </Heading3>
 
+            <p className='p-1 text-lg'>Será en esta ciudad:</p>
+            <p><strong>{city}</strong></p>
+            <p className='p-1 text-lg'>En este lugar:</p>
+            <p><strong>{placeName}</strong></p>
+            <p className='p-1 text-lg'>Te dejamos la dirección a mano:</p>
+            <p><strong>{address}</strong></p>
+
+            <Heading3 className='mt-4'>Mapa</Heading3>
             {
                 (longitude && latitude) ? (
-                    <div style={{ width: '100%', height: '400px' }}>
-                        <iframe
-                            src={`https://www.google.com/maps?q=${latitude},${longitude}&z=15&output=embed`}
-                            width="100%"
-                            height="100%"
-                            style={{ border: 0 }}
-                            allowFullScreen
-                            loading="lazy"
-                        ></iframe>
-                    </div>
+                    <>
 
+                        <div className='mt-4' style={{ width: '100%', height: '400px' }}>
+                            <iframe
+                                src={`https://www.google.com/maps?q=${latitude},${longitude}&z=15&output=embed`}
+                                width="100%"
+                                height="100%"
+                                style={{ border: 0 }}
+                                allowFullScreen
+                                loading="lazy"
+                            ></iframe>
+                        </div>
+                    </>
                 ) : (
                     <p className="w-full text-center text-sm text-muted-foreground mt-4">
-                        Ubicación no disponible
+                        Ubicación en mapa no disponible
                     </p>
                 )
             }
