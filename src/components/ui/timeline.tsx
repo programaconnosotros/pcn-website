e68301b79dcae2a68658a48d8ad0554bfee9111e
MauriJC <xaviermauricio33@gmail.com>
2025-01-22 14:00:50 +0000

    chore(format): formated all files

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useScroll, useTransform, motion } from 'framer-motion';
+import { useMotionValueEvent, useScroll, useTransform, motion } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 
 interface TimelineEntry {
