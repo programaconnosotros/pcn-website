00dff3518382e8322152d55ef301c41ae58d8319
MauriJC <xaviermauricio33@gmail.com>
2025-01-28 15:24:39 +0000

    chore(EventDetails): removed unnecesary return statement

--- src/components/events/event-details.tsx
+++ src/components/events/event-details.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Heading3 } from '../ui/heading-3';
 
-const EventDetails: React.FC<{ description: string }> = ({ description }) => {
-  return (
-    <>
-      <Heading3 className="mb-4 mt-4 text-2xl font-semibold">Detalles del evento</Heading3>
+const EventDetails: React.FC<{ description: string }> = ({ description }) => (
+  <>
+    <Heading3 className="mb-4 mt-4 text-2xl font-semibold">Detalles del evento</Heading3>
 
-      <p className="mt-2 text-center text-lg">{description}</p>
-    </>
-  );
-};
+    <p className="mt-2 text-center text-lg">{description}</p>
+  </>
+);
 
 export default EventDetails;
