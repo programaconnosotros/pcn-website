651d06508f4ef58ac12651e537c373b62d2f7414
MauriJC <xaviermauricio33@gmail.com>
2025-01-28 15:43:32 +0000

    chore(EventLocation): removed unnecesary return statement

--- src/components/events/event-location.tsx
+++ src/components/events/event-location.tsx
@@ -15,45 +15,43 @@ const EventLocation: React.FC<EventLocationProps> = ({
   city,
   address,
   placeName,
-}) => {
-  return (
-    <div>
-      <Heading3 className="mb-4 mt-4 text-2xl font-semibold">Ubicación del evento</Heading3>
+}) => (
+  <div>
+    <Heading3 className="mb-4 mt-4 text-2xl font-semibold">Ubicación del evento</Heading3>
 
-      <p className="p-1 text-lg">Será en esta ciudad:</p>
-      <p>
-        <strong>{city}</strong>
-      </p>
-      <p className="p-1 text-lg">En este lugar:</p>
-      <p>
-        <strong>{placeName}</strong>
-      </p>
-      <p className="p-1 text-lg">Te dejamos la dirección a mano:</p>
-      <p>
-        <strong>{address}</strong>
-      </p>
+    <p className="p-1 text-lg">Será en esta ciudad:</p>
+    <p>
+      <strong>{city}</strong>
+    </p>
+    <p className="p-1 text-lg">En este lugar:</p>
+    <p>
+      <strong>{placeName}</strong>
+    </p>
+    <p className="p-1 text-lg">Te dejamos la dirección a mano:</p>
+    <p>
+      <strong>{address}</strong>
+    </p>
 
-      <Heading3 className="mt-4">Mapa</Heading3>
-      {longitude && latitude ? (
-        <>
-          <div className="mt-4" style={{ width: '100%', height: '400px' }}>
-            <iframe
-              src={`https://www.google.com/maps?q=${latitude},${longitude}&z=15&output=embed`}
-              width="100%"
-              height="100%"
-              style={{ border: 0 }}
-              allowFullScreen
-              loading="lazy"
-            ></iframe>
-          </div>
-        </>
-      ) : (
-        <p className="mt-4 w-full text-center text-sm text-muted-foreground">
-          Ubicación en mapa no disponible
-        </p>
-      )}
-    </div>
-  );
-};
+    <Heading3 className="mt-4">Mapa</Heading3>
+    {longitude && latitude ? (
+      <>
+        <div className="mt-4" style={{ width: '100%', height: '400px' }}>
+          <iframe
+            src={`https://www.google.com/maps?q=${latitude},${longitude}&z=15&output=embed`}
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen
+            loading="lazy"
+          ></iframe>
+        </div>
+      </>
+    ) : (
+      <p className="mt-4 w-full text-center text-sm text-muted-foreground">
+        Ubicación en mapa no disponible
+      </p>
+    )}
+  </div>
+);
 
 export default EventLocation;
