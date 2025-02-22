ad6752f56d94d6109c5b7afa3e225cc12783479d
agustin-sanc <js.agustin.sz@gmail.com>
2025-02-22 19:52:00 +0000

    feat: remove text

--- src/components/landing/scroll-arrow.tsx
+++ src/components/landing/scroll-arrow.tsx
@@ -2,7 +2,6 @@ import { ChevronDown } from 'lucide-react';
 
 export const ScrollArrow = () => (
   <div className="absolute bottom-8 flex transform animate-bounce flex-col items-center gap-2">
-    <p className="text-xl text-white">Scrolleá para ver más</p>
     <ChevronDown className="h-8 w-8 text-white" />
   </div>
 );
