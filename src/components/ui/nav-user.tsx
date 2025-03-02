b089f169d494c223e9ea61f705f5edae284907c5
agustin-sanc <js.agustin.sz@gmail.com>
2025-03-02 23:34:35 +0000

    refactor: use image instead of avatar

--- src/components/ui/nav-user.tsx
+++ src/components/ui/nav-user.tsx
@@ -25,7 +25,7 @@ export function NavUser({
   user: {
     name: string;
     email: string;
-    avatar: string;
+    image: string;
   };
 }) {
   const { isMobile } = useSidebar();
@@ -40,7 +40,7 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage src={user.image} alt={user.name} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
