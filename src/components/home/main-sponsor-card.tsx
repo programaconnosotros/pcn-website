8a8bd04a0a271d12aa96cae886fdbbe4558cd6eb
agustin-sanc <js.agustin.sz@gmail.com>
2025-03-03 20:44:17 +0000

    feat: add asz-software as main sponsor

--- src/components/home/main-sponsor-card.tsx
+++ src/components/home/main-sponsor-card.tsx
@@ -1,29 +1,15 @@
 'use client';
 
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from '@components/ui/card';
-import { Button } from '@components/ui/button';
-import { HandHeart } from 'lucide-react';
 import Link from 'next/link';
+import ASZSoftwareLogo from './asz-software-logo';
 
 export const MainSponsorCard = () => (
-  <Card>
-    <CardHeader>
-      <CardTitle>Sponsors</CardTitle>
-
-      <CardDescription className="mt-3">
-        Estamos en búsqueda de sponsors para este proyecto. Podemos mostrar tu logo aquí y tu
-        sitio web, para que miles de profesionales y estudiantes de la industria del software lo
-        vean y sepan de tu interés en apoyar este tipo de iniciativas.
-      </CardDescription>
-    </CardHeader>
-
-    <CardContent className="flex flex-col">
-      <Link className="block" href="https://wa.me/5493815777562">
-        <Button className="mt-4 flex w-full flex-row items-center gap-2">
-          Quiero ser sponsor
-          <HandHeart className="h-5 w-5" />
-        </Button>
-      </Link>
-    </CardContent>
-  </Card>
+  <Link
+    href="https://asz.software"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="w-full rounded-md border"
+  >
+    <ASZSoftwareLogo />
+  </Link>
 );
