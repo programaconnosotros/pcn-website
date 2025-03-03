c1984b5c74c8b50053305cbf908ee38da4681447
agustin-sanc <js.agustin.sz@gmail.com>
2025-03-03 20:44:17 +0000

    feat: add asz-software as main sponsor

---
+++ src/components/home/asz-software-logo.tsx
@@ -0,0 +1,21 @@
+import { TextGenerateEffect } from '../ui/text-generate-effect';
+
+const ASZSoftwareLogo = () => (
+  //   <div className="relative z-10 flex w-full flex-col items-center">
+  <div className="flex w-full flex-col items-center bg-black">
+    <div className="mb-4 mt-4 flex flex-row items-center">
+      <img src="/asz-software-logo-2.webp" alt="ASZ Studio" className="-ml-8 h-24" />
+
+      <h1 className="bg-gradient-to-b from-white to-gray-500 bg-clip-text text-4xl font-bold text-transparent">
+        ASZ Software
+      </h1>
+    </div>
+
+    <TextGenerateEffect
+      className="max-w-xl px-6 text-center md:px-0 md:text-2xl"
+      words={'Top-quality software engineering studio.'}
+    />
+  </div>
+);
+
+export default ASZSoftwareLogo;
