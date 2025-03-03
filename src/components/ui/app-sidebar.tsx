0cc67ac027208ec09b8d44017cb56eb82f204879
Matias Daniel Gutierrez <62714325+MatiasDG539@users.noreply.github.com>
2025-03-03 15:39:32 -0300

    [PCN - #15] - Recreate Sidebar (#19)
    * feat: add shadcn sidebar
    
    * chore: add commands to generate prisma
    
    * refactor: format
    
    * chore: remove next auth endpoints
    
    * chore: remove accounts from database
    
    * chore: remove session logic in hero
    
    * feat: remove text
    
    * feat: fix auth
    
    * feat: fix auth
    
    * chore: install node, pnpm and deploy prisma migrations
    
    * chore: add directUrl
    
    * fix: fetch user from db
    
    * refactor: format with prettier
    
    * feat: add layout provider and fix cookie name
    
    * fix: user type
    
    * feat: add new example sidebar
    
    * feat: make name required
    
    * chore: remove unused env vars
    
    * chore: add reset-database command
    
    * refactor: use image instead of avatar
    
    * refactor: remove used from data
    
    * feat: improve user section
    
    * refactor: remove unused imports
    
    * fix: types and navbar sections
    
    * feat: add sign-out
    
    * chore: remove unused sign-out component
    
    * fix: profile url and sign out problem
    
    * feat: add logo in sidebar
    
    * feat: add theme toggle in sidebar
    
    * feat: allow sidebar to work without user logged in
    
    * feat: progress with sidebar trigger
    
    * feat: fix navbar for mobile
    
    * fix: padding top for children
    
    * feat: use heading2 for title
    
    * feat: add whatsapp and discord to sidebar
    
    * fix: type issue due to old version of prisma
    
    * refactor: use User from prisma client
    
    * chore: remove zod generator
    
    ---------
    
    Co-authored-by: agustin-sanc <js.agustin.sz@gmail.com>

---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,165 @@
+'use client';
+
+import * as React from 'react';
+import {
+  BookOpen,
+  Command,
+  Home,
+  Instagram,
+  Laptop,
+  LifeBuoy,
+  Linkedin,
+  Send,
+  SquareTerminal,
+  Youtube,
+  Calendar,
+  Image,
+} from 'lucide-react';
+
+import { NavMain } from '@/components/ui/nav-main';
+import { NavProjects } from '@/components/ui/nav-projects';
+import { NavSecondary } from '@/components/ui/nav-secondary';
+import { NavUser } from '@/components/ui/nav-user';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { User } from '@prisma/client';
+
+const data = {
+  navMain: [
+    {
+      title: 'Inicio',
+      url: '/home',
+      icon: Home,
+    },
+    {
+      title: 'Actividades',
+      url: '/events',
+      icon: Calendar,
+    },
+    {
+      title: 'Consejos',
+      url: '/advises',
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: 'Fotos',
+      url: '/photos',
+      icon: Image,
+    },
+    {
+      title: 'Charlas',
+      url: '/talks',
+      icon: BookOpen,
+    },
+    {
+      title: 'Cursos',
+      url: '/courses',
+      icon: Laptop,
+      items: [
+        {
+          title: 'Vim',
+          url: '/courses/vim',
+        },
+        {
+          title: 'LaTeX',
+          url: '/courses/latex',
+        },
+        {
+          title: 'Git & GitHub',
+          url: '/courses/git-and-github',
+        },
+        {
+          title: 'Ver todos',
+          url: '/courses',
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: 'Soporte',
+      url: 'https://wa.me/5493815777562',
+      icon: LifeBuoy,
+    },
+    {
+      title: 'Feedback',
+      url: 'https://wa.me/5493815777562',
+      icon: Send,
+    },
+  ],
+  socialNetworks: [
+    {
+      name: 'WhatsApp',
+      url: 'https://chat.whatsapp.com/IFwKhHXoMwM6ysKcbfHiEh',
+      icon: Send,
+    },
+    {
+      name: 'Discord',
+      url: 'https://discord.gg/dTQexKw56S',
+      icon: Send,
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/programa.con.nosotros/',
+      icon: Instagram,
+    },
+    // {
+    //   name: 'X',
+    //   url: 'https://x.com/programaconnos',
+    //   icon: X,
+    // },
+    {
+      name: 'YouTube',
+      url: 'https://www.youtube.com/@programaconnosotros',
+      icon: Youtube,
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/company/programaconnosotros/',
+      icon: Linkedin,
+    },
+  ],
+};
+
+interface AppSidebarProps extends React.ComponentProps<typeof Sidebar> {
+  user: User | null;
+}
+
+export function AppSidebar({ user, ...props }: AppSidebarProps) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <a href="#">
+                <div className="text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg bg-black p-1">
+                  <img src="/logo.png" alt="programaConNosotros" />
+                </div>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">programaConNosotros</span>
+                </div>
+              </a>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavProjects socialNetworks={data.socialNetworks} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={user} />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
