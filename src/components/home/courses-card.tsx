af396309030437495a9311746964357b4df4b4b3
agustin-sanc <js.agustin.sz@gmail.com>
2025-03-03 21:15:17 +0000

    fix: don't send description to statcard

--- src/components/home/courses-card.tsx
+++ src/components/home/courses-card.tsx
@@ -2,11 +2,5 @@ import { Presentation } from 'lucide-react';
 import { StatCard } from './stat-card';
 
 export const CoursesCard = () => (
-  <StatCard
-    href="/courses"
-    title="Cursos"
-    Icon={Presentation}
-    value={6}
-    description="Estamos armando un catálogo de cursos muy interesantes!"
-  />
+  <StatCard href="/courses" title="Cursos" Icon={Presentation} value={6} />
 );
--- src/components/home/talks-card.tsx
+++ src/components/home/talks-card.tsx
@@ -3,11 +3,5 @@ import { StatCard } from './stat-card';
 import { talks } from '@/app/(platform)/talks/talks';
 
 export const TalksCard = () => (
-  <StatCard
-    href="/talks"
-    title="Charlas"
-    Icon={MicVocal}
-    value={talks.length}
-    description="Cada vez son más los miembros que se animan a dar charlas!"
-  />
+  <StatCard href="/talks" title="Charlas" Icon={MicVocal} value={talks.length} />
 );
