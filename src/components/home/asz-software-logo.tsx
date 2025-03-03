2bedcf89220e41ddc59b4b77c8c1c3ec55633a79
agustin-sanc <js.agustin.sz@gmail.com>
2025-03-03 21:05:28 +0000

    feat: decrease asz software logo size

--- src/components/home/asz-software-logo.tsx
+++ src/components/home/asz-software-logo.tsx
@@ -4,7 +4,7 @@ const ASZSoftwareLogo = () => (
   //   <div className="relative z-10 flex w-full flex-col items-center">
   <div className="flex w-full flex-col items-center bg-black">
     <div className="mb-4 mt-4 flex flex-row items-center">
-      <img src="/asz-software-logo-2.webp" alt="ASZ Studio" className="-ml-8 h-24" />
+      <img src="/asz-software-logo-2.webp" alt="ASZ Studio" className="-ml-8 h-12" />
 
       <h1 className="bg-gradient-to-b from-white to-gray-500 bg-clip-text text-4xl font-bold text-transparent">
         ASZ Software
