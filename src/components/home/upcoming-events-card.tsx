f1d5e91417fde30ab2f2110441d21e41f3d85cbf
agustin-sanc <js.agustin.sz@gmail.com>
2025-03-03 20:46:19 +0000

    feat: minify home cards

--- src/components/home/upcoming-events-card.tsx
+++ src/components/home/upcoming-events-card.tsx
@@ -2,11 +2,5 @@ import { Calendar } from 'lucide-react';
 import { StatCard } from './stat-card';
 
 export const UpcomingEventsCard = () => (
-  <StatCard
-    href="/events"
-    title="Próximos eventos"
-    Icon={Calendar}
-    value={3}
-    description="Organizamos eventos para que podamos aprender y crecer juntos."
-  />
+  <StatCard href="/events" title="Próximos eventos" Icon={Calendar} value={3} />
 );
