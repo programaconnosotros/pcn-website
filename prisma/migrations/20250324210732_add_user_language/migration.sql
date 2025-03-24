4903943dc65f8195140de24748f18010918be1fa
FacuBzn <juanfacundobazan@gmail.com>
2025-03-24 21:08:27 +0000

    refactor: remove favoriteProgrammingLanguage column and update UserLanguage schema to include color and logo fields

--- prisma/migrations/20250324210732_add_user_language/migration.sql
+++ prisma/migrations/20250324210732_add_user_language/migration.sql
@@ -1,14 +1,27 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `favoriteProgrammingLanguage` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "favoriteProgrammingLanguage";
+
 -- CreateTable
 CREATE TABLE "UserLanguage" (
     "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
     "language" TEXT NOT NULL,
-    "proficiency" INTEGER NOT NULL,
+    "color" TEXT NOT NULL,
+    "logo" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "UserLanguage_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE INDEX "UserLanguage_userId_idx" ON "UserLanguage"("userId");
 
 -- AddForeignKey
 ALTER TABLE "UserLanguage" ADD CONSTRAINT "UserLanguage_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
