e3cb45928289590f2499c6d1954ce474e51e4932
nicolas Fuentes <nicolasfuentesg06@outlook.com>
2025-03-31 14:08:46 -0300

    feat(photos): add photo section with masonry layout

--- src/app/(platform)/photos/page.tsx
+++ src/app/(platform)/photos/page.tsx
@@ -1,9 +1,12 @@
 import { Heading2 } from '@/components/ui/heading-2';
+import { Gallery } from '@components/photoGallery/gallery';
 
 const Photos = () => (
-  <div className="mt-4 md:px-20">
-    <Heading2>Fotos</Heading2>
-    <div className="mt-4">Próximamente.</div>
+  <div className="mt-4 w-screen px-4 md:px-20">
+    <div className="flex flex-col">
+      <Heading2>Fotos</Heading2>
+      <Gallery />
+    </div>
   </div>
 );
 
