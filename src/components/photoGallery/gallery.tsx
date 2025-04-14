320484c794f6db563a6aec4bc6a2be3650311e71
agustin-sanc <js.agustin.sz@gmail.com>
2025-04-14 01:26:10 +0000

    feat: remove blank space between pictures

--- src/components/photoGallery/gallery.tsx
+++ src/components/photoGallery/gallery.tsx
@@ -15,19 +15,82 @@ export const Gallery = () => {
     { id: '13', src: '/gallery-photos/tobias-watch-pc.webp' },
   ];
 
+  // Dividir las imágenes en columnas
+  const getColumnImages = (columnIndex: number, totalColumns: number) => {
+    return images.filter((_, index) => index % totalColumns === columnIndex);
+  };
+
   return (
-    <section className="flex max-w-[1248px] flex-wrap">
-      {images.map((photo, key) => (
-        <div key={key} className="w-full p-0 md:w-1/2 lg:w-1/3">
+    <section className="flex max-w-[1248px] gap-0">
+      {/* Columna móvil */}
+      <div className="flex w-full flex-col gap-0 md:hidden">
+        {images.map((photo, key) => (
           <img
+            key={key}
             src={photo.src}
             alt={`Imagen ${photo.id}`}
-            width={350}
-            height={300}
-            className="h-auto w-full object-cover group-hover:opacity-75"
+            className="h-auto w-full object-cover"
           />
+        ))}
+      </div>
+
+      {/* Columnas tablet (2 columnas) */}
+      <div className="hidden w-full gap-0 md:flex lg:hidden">
+        <div className="flex w-1/2 flex-col gap-0">
+          {getColumnImages(0, 2).map((photo, key) => (
+            <img
+              key={key}
+              src={photo.src}
+              alt={`Imagen ${photo.id}`}
+              className="h-auto w-full object-cover"
+            />
+          ))}
         </div>
-      ))}
+        <div className="flex w-1/2 flex-col gap-0">
+          {getColumnImages(1, 2).map((photo, key) => (
+            <img
+              key={key}
+              src={photo.src}
+              alt={`Imagen ${photo.id}`}
+              className="h-auto w-full object-cover"
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Columnas desktop (3 columnas) */}
+      <div className="hidden w-full gap-0 lg:flex">
+        <div className="flex w-1/3 flex-col gap-0">
+          {getColumnImages(0, 3).map((photo, key) => (
+            <img
+              key={key}
+              src={photo.src}
+              alt={`Imagen ${photo.id}`}
+              className="h-auto w-full object-cover"
+            />
+          ))}
+        </div>
+        <div className="flex w-1/3 flex-col gap-0">
+          {getColumnImages(1, 3).map((photo, key) => (
+            <img
+              key={key}
+              src={photo.src}
+              alt={`Imagen ${photo.id}`}
+              className="h-auto w-full object-cover"
+            />
+          ))}
+        </div>
+        <div className="flex w-1/3 flex-col gap-0">
+          {getColumnImages(2, 3).map((photo, key) => (
+            <img
+              key={key}
+              src={photo.src}
+              alt={`Imagen ${photo.id}`}
+              className="h-auto w-full object-cover"
+            />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
