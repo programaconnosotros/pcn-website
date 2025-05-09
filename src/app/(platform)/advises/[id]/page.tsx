68152bbc20123951cc56a3bb4891151754007e29
agustin-sanc <js.agustin.sz@gmail.com>
2025-05-09 23:52:13 +0000

    feat: add button to return to all advises

--- src/app/(platform)/advises/[id]/page.tsx
+++ src/app/(platform)/advises/[id]/page.tsx
@@ -1,11 +1,15 @@
 import { getAdviseById } from '@/actions/advises/get-advise';
 import { AdviseCard } from '@/components/advises/advise-card';
 import { CommentSection } from '@/components/advises/comment-section';
+import { Button } from '@/components/ui/button';
 import prisma from '@/lib/prisma';
+import { ArrowLeftIcon } from 'lucide-react';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 
 export default async function AdvisePage({ params }: { params: { id: string } }) {
   const sessionId = cookies().get('sessionId');
+
   const session = sessionId
     ? await prisma.session.findUnique({
         where: { id: sessionId.value },
@@ -21,6 +25,14 @@ export default async function AdvisePage({ params }: { params: { id: string } })
 
   return (
     <div className="mt-4 md:max-w-screen-xl md:px-20">
+      <div className="mb-4">
+        <Link href="/advises">
+          <Button variant="outline">
+            <ArrowLeftIcon className="mr-2 h-4 w-4" /> Volver a todos los consejos
+          </Button>
+        </Link>
+      </div>
+
       <AdviseCard advise={advise} session={session} />
       <CommentSection adviseId={advise.id} comments={advise.comments ?? []} session={session} />
     </div>
