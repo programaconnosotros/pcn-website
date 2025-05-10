c6efe352b68e976821c329e477328705755aaf64
agustin-sanc <js.agustin.sz@gmail.com>
2025-05-10 02:59:14 +0000

    refactor: remove comments

--- src/app/(platform)/photos/page.tsx
+++ src/app/(platform)/photos/page.tsx
@@ -1,17 +1,3 @@
-// import { Heading2 } from '@/components/ui/heading-2';
-// import { Gallery } from '@/components/photo-gallery/gallery';
-
-// const Photos = () => (
-//   <div className="mt-4 w-full px-4 md:px-20">
-//     <div className="flex flex-col">
-//       <Heading2>Fotos</Heading2>
-//       <Gallery />
-//     </div>
-//   </div>
-// );
-
-// export default Photos;
-
 'use client';
 
 import { useSearchParams } from 'next/navigation';
