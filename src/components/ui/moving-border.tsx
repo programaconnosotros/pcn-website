49de3584dbc52d73afeff1ce32c51ed2370c8796
agustin-sanc <js.agustin.sz@gmail.com>
2025-05-10 00:53:32 +0000

    fix: motion import

--- src/components/ui/moving-border.tsx
+++ src/components/ui/moving-border.tsx
@@ -6,7 +6,7 @@ import {
   useMotionTemplate,
   useMotionValue,
   useTransform,
-} from 'framer-motion';
+} from 'motion/react';
 import { useRef } from 'react';
 import { cn } from '@/lib/utils';
 
