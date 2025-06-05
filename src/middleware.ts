845f0bcfd7d3a5cbccb91e3296bc2a269022a17b
agustin-sanc <js.agustin.sz@gmail.com>
2025-06-05 19:42:26 +0000

    multiple fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const sessionId = request.cookies.get('sessionId')?.value;
+  // const sessionId = request.cookies.get('sessionId')?.value;
 
-  if (!sessionId) {
-    return NextResponse.redirect(new URL('/autenticacion/iniciar-sesion', request.url));
-  }
+  // if (!sessionId) {
+  //   return NextResponse.redirect(new URL('/autenticacion/iniciar-sesion', request.url));
+  // }
 
   return NextResponse.next();
 }
