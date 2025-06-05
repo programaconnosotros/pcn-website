5555caf029a3462fb065ae3ed539a6bfb086de9d
agustin-sanc <js.agustin.sz@gmail.com>
2025-06-05 18:17:11 +0000

    feat: update pcn story

--- src/app/(platform)/historia/page.tsx
+++ src/app/(platform)/historia/page.tsx
@@ -5,10 +5,8 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from '@/components/ui/breadcrumb';
-import { Button } from '@/components/ui/button';
 import { Heading2 } from '@/components/ui/heading-2';
 import { Heading3 } from '@/components/ui/heading-3';
-import Link from 'next/link';
 
 const PCN = () => (
   <code className="font-bold text-green-700 dark:text-pcnGreen">programaConNosotros</code>
@@ -33,189 +31,173 @@ const PCNStory = () => (
     <Heading2>Historia de programaConNosotros</Heading2>
 
     <p>
-      Hola! Soy <b>Agustín Sánchez</b>, uno de los fundadores de nuestra comunidad, y en este
-      artículo quiero contarte cómo llegamos a crear <PCN />, y casi todos los pasos que dimos para
-      ser lo que somos hoy. Espero que te guste!
-    </p>
-
-    <Heading3>
-      Todo empezó en la{' '}
-      <Link
-        href="https://www.frt.utn.edu.ar/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:underline"
-      >
-        UTN-FRT
-      </Link>
-    </Heading3>
-
-    <p>
-      Empecé a estudiar <u>ingeniería en sistemas de información</u> en la UTN (Universidad
-      Tecnológica Nacional), en la provincia de Tucumán, Argentina. Lo que más me gustaba de la
-      carrera era programar y desarrollar software, asi que empecé a moverme extracurricularmente
-      para aprender mucho más sobre eso.
+      Hola! Gracias por visitar la historia de la comunidad, esperamos te sirva para entender el
+      contexto y los valores de lo que creamos, y también motivarte a colaborar en la comunidad y
+      compartir la carrera para ayudarnos!
     </p>
 
     <p>
-      En la facultad conocí a <b>Germán Navarro</b>, <b>Chelo Núñez</b> e <b>Iván Taddei</b>,
-      compañeros que fueron muy importantes para mi formación como profesional y como persona.
-      Luego, tuve la fortuna de que mis hermanos menores, <b>Mauricio</b> y <b>Esteban</b>, también
-      empezaron a estudiar la misma carrera. Los 6 formamos un grupo de amigos excelente.
-    </p>
-
-    <Heading3>
-      Fuimos voluntarios en el{' '}
-      <Link
-        href="https://ieee.org/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:underline"
-      >
-        IEEE
-      </Link>
-    </Heading3>
-
-    <p>
-      Con Germán pudimos entrar a formar parte como voluntarios en el IEEE (instituto más
-      prestigioso de ingeniería del mundo). Ahí aprendimos muchas cosas, conocimos personas muy
-      copadas y empezamos a organizar actividades para compartir conocimiento y facilitar el
-      networking.
+      programaConNosotros es una comunidad de profesionales y estudiantes de ingeniería de software,
+      fundada en el año 2020 en Tucumán (Argentina), en plena cuarentena por la pandemia del
+      COVID-19. Los co-fundadores son los hermanos Sánchez (Agustín, Mauricio y Esteban) junto a
+      Germán Navarro, Marcelo Núñez e Iván Taddei.
     </p>
 
     <p>
-      Me encantaba organizar cosas y liderar el equipo de voluntarios, llegué a ser presidente
-      primero del <b>Capítulo Estudiantil de IEEE Computer Society de la UNT</b> por 2 años
-      consecutivos, y luego, presidente de la <b>Rama Estudiantil IEEE de la UTN-FRT</b>. Germán fue
-      vicepresidente en ambos grupos.
+      Si bien la comunidad fue fundada en 2020, los co-fundadores y las personas más destacadas de
+      la comunidad se conocieron varios años atrás, y queremos contar toda la historia.
+    </p>
+
+    <Heading3>Comienzos en la UTN-FRT</Heading3>
+
+    <p>
+      En el año 2015, en la Universidad Tecnológica de Tucumán (Argentina), se conocieron Agustín
+      Sánchez y Germán Navarro, estudiando Ingeniería en Sistemas de Información. La gran pasión que
+      tenían por la ingeniería de software, particularmente por la programación, los llevó a
+      estudiar mucho más de lo que se enseñaba en la universidad y a participar de actividades
+      extracurriculares como charlas y competencias de programación.
+    </p>
+
+    <Heading3>Voluntariado en el IEEE</Heading3>
+
+    <p>
+      En el año 2017, Agus y Germán se sumaron al IEEE para participar del programa de voluntariado,
+      que tiene como objetivo hacer networking (hacer contactos, conocer gente, etc) y organizar
+      eventos técnicos.
     </p>
 
     <p>
-      Las actividades más piolas que organizamos fueron:
-      <ul className="my-6 ml-6 list-disc [&>li]:mt-2">
-        <li>
-          <b>Tucumán Hack Weekend</b>: fue un congreso de seguridad informática de 2 días. Tuvimos
-          speakers de primer nivel, como <b>Victor Figueredo</b> (líder y fundador de{' '}
-          <b>Tucumán Hacking</b>) y <b>Facundo Gelatti</b> (ingeniero de software en <b>10Pines</b>,
-          docente en la universidad de Quilmes y referente de ingeniería de software a nivel
-          nacional). También me animé a dar mi primera charla, hablé sobre programación competitiva.
-        </li>
-
-        <li>
-          <b>Code Warfare</b>: fue una competencia de programación que organizamos en un laboratorio
-          de computación de la UTN-FRT. Participaron más de 10 equipos, de diferentes universidades
-          de Tucumán y estuvo muy divertido.
-        </li>
-
-        <li>
-          <b>WESCIS</b>: fue un congreso de 3 días sobre diferentes ingenierías. Uno de esos días
-          fue sobre computación, del cual tuve el orgullo de ser el presidente de la organización.
-          Fue realizado en la <b>UNSTA</b> de Yerba Buena, y pudimos traer speakers excelentes, como{' '}
-          <b>Hernán Wilkinson</b> (CEO de <b>10Pines</b> y referente a nivel mundial en ingeniería
-          de software), <b>Alejandro Bianchi</b> (CEO de <b>Liveware</b> y referente de arquitectura
-          de software en latinoamérica) y el crack ya mencionado, <b>Facundo Gelatti</b>.{' '}
-        </li>
-
-        <li>
-          <b>Club de algoritmos</b>: fue un grupo de programación que cree para que muchas personas
-          puedan aprender algoritmos y estructuras de datos. Los viernes, luego de las clases de la
-          carrera, nos quedábamos en un laboratorio de la facultad y daba las clases sobre
-          diferentes temas relacionados a algoritmos, estructuras de datos, patrones y demás.
-        </li>
-      </ul>
-    </p>
-
-    <Heading3>Necesitábamos más</Heading3>
-
-    <p>
-      El IEEE es un instituto que engloba demasiadas disciplinas de ingeniería, pero nosotros
-      necesitamos algo más específico para la ingeniería de software, asi que creamos...
-    </p>
-
-    <Heading3>
-      <PCN />
-    </Heading3>
-
-    <p>
-      Una comunidad para personas apasionadas por la ingeniería de software, que quieren estar
-      constantemente actualizados, mejorando, compartiendo conocimientos y contactos con los demás.
+      Viajaron a un evento en Catamarca llamado Reunión Nacional de Ramas (RNR), y se hicieron
+      amigos de Facu Gelatti y Franco Mirada. Facu y Franco ya estaban terminando de cursar la
+      carrera y fueron mentores muy importantes. Fue difícil para Agus y Germán romper el hielo y
+      animarse a conversar con personas más experimentadas en la industria del software, y es algo
+      por lo que la mayoría de las personas de la industria transitan. No conocer gente, no poder
+      recibir consejos, ayuda, guía, etc. PCN es una comunidad que permite conseguirlo muy
+      fácilmente.
     </p>
 
     <p>
-      <PCN /> es un lugar en el que podés encontrar a a un experto en ingeniería de software
-      charlando con una persona que recién está empezando su camino dentro del software. Hay muchos
-      mentores dispuestos a dar los mejores consejos, enseñar cosas difíciles de forma accesible y
-      muchas cosas más, por el simple hecho de que estamos apasionados por el software y creemos que
-      ayudarnos entre todos, podemos llegar más lejos.
+      Agus y Germán empezaron a ayudar en la organización de algunas actividades técnicas dentro del
+      IEEE. Facu y Franco ya venían ayudando desde hacía algunos años. Facu daba algunos talleres y
+      charlas. Agus y Germán por su lado dieron su primera charla en septiembre del 2017:
+      Introducción a la Programación Competitiva.
     </p>
 
-    <Heading3>Las Lightning Talks</Heading3>
+    {/* // TODO: Agregar flyer de la charla */}
 
     <p>
-      En el año 2020, empezamos a organizar una actividad llamada Lightning Talks, que consiste en
-      presentaciones rápidas (5-10 minutos) sobre un tema que te apasiona. Esto permite a los
-      oyentes no aburrirse y a los speakers animarse a dar charlas. Con las Lightning Talks,
-      empezamos a atraer mucha gente. Al principio las hacíamos online por Discord y YouTube, porque
-      estábamos en pandemia y no se podía organizar actividades de forma presencial. Actualmente,
-      organizamos varias ediciones presenciales de la actividad y cada vez se copa más gente.
+      Les apasionaba mucho la programación competitiva asi que daban clases ad-honorem sobre
+      algoritmos y estructuras de datos avanzadas aparte de cursar la carrera de ingeniería. A estas
+      clases se sumaban varios estudiantes apasionados por la programación. El profesor Augusto
+      Nasrallah y el ingeniero Jorge Buabud de la UTN-FRT colaboraban prestando un laboratorio de
+      computación de la universidad para dictar estas clases.
     </p>
 
-    <div className="my-6 grid grid-cols-1 gap-4 md:grid-cols-3">
-      <div className="mb-4 md:mb-0">
-        <img
-          src="/lightning-talks/arquitectura-software.webp"
-          alt="Arquitectura de software"
-          className="w-full rounded-lg object-cover md:h-48"
-        />
-
-        <p className="mt-2 text-xs text-gray-500">
-          Yo dando una charla de arquitectura de software.
-        </p>
-      </div>
-
-      <div className="mb-4 md:mb-0">
-        <img
-          src="/lightning-talks/arquitectura-modular.webp"
-          alt="Arquitectura modular"
-          className="w-full rounded-lg object-cover md:h-48"
-        />
-
-        <p className="mt-2 text-xs text-gray-500">
-          Mauricio dando una charla de arquitectura modular.
-        </p>
-      </div>
-
-      <div className="mb-4 md:mb-0">
-        <img
-          src="/lightning-talks/paradigmas.webp"
-          alt="Paradigmas de programación"
-          className="w-full rounded-lg object-cover md:h-48"
-        />
-
-        <p className="mt-2 text-xs text-gray-500">
-          Chelo dando una charla de paradigmas de programación.
-        </p>
-      </div>
-    </div>
-
-    <Link href="/talks">
-      <Button className="w-full" variant="outline">
-        Ver el historial de charlas dadas en PCN
-      </Button>
-    </Link>
-
-    <Heading3>Empezamos a juntarnos más seguido para networking</Heading3>
-
-    <img src="/juntada.webp" alt="Networking" className="w-full rounded-lg object-cover" />
-
-    <Heading3>Empecé a desarrollar este website</Heading3>
+    <Heading3>Code Warfare</Heading3>
 
     <p>
-      En el año 2024, cree este sitio web para que podamos compartir conocimiento y encontrar
-      información copada acerca de ingeniería de software. Este proyecto es open source y cualquier
-      persona puede contribuir si lo desea, ya sea para mejorar cosas, fixear bugs o agregar
-      features. Uno de los objetivos de este proyecto es facilitar que las personas puedan ganar
-      experiencia en diferentes disciplinas.
+      A finales de 2017, organizaron un torneo de programación en la UTN de Tucumán, lo llamaron
+      Code Warfare. Se anotaron muchos equipos y estuvo muy divertido.
+    </p>
+
+    {/* TODO: Agregar fotos del Code Warfare. */}
+
+    <Heading3>IEEE Computer Society</Heading3>
+
+    <p>
+      En el año 2018, Agus y Germán dieron un paso importante presidiendo el Capítulo Estudiantil de
+      IEEE Computer Society en la Universidad Nacional de Tucumán.
+    </p>
+
+    <Heading3>Actividades con Tucumán Hacking</Heading3>
+
+    <p>
+      En abril del 2018, organizaron el Tucumán Hack Weekend, un congreso internacional de seguridad
+      informática llevado a cabo en la UTN de Tucumán. El evento fue organizado también por la
+      organización Tucumán Hacking, presidida por Victor Figueredo (ahora CEO de Endpoint
+      Consulting).
+    </p>
+
+    {/* // TODO: Agregar fotos del Tucumán Hack Weekend. */}
+
+    <p>
+      Organizaron varios talleres de seguridad informática en conjunto con la organización Tucumán
+      Hacking, destacándose los talleres de pentesting y hacking ofensivo.
+    </p>
+    {/* // TODO: Agregar fotos de los cursos de seguridad informática */}
+
+    <Heading3>Nibble</Heading3>
+
+    <p>
+      En el año 2019, Agus y Germán conocieron 2 personas en la UTN-FRT que se convirtieron en
+      grandes compañeros y amigos: Iván Taddei y Marcelo “Chelo” Núñez. El grupo de los 4, bautizado
+      por el Chelo como “Nibble”, fue un grupo destacado en la facultad, más que nada por la pasión
+      que tenían por la ingeniería del software. Nunca fueron a la facultad solo para aprobar
+      materias y ya, sino que realmente querían entender cómo funcionan las cosas, cómo se
+      relacionan, cómo se aplican, y estudiar mucho más de lo que adentro de las aulas se enseñaba.
+    </p>
+
+    <Heading3>El nacimiento de programaConNosotros</Heading3>
+
+    <p>
+      En el año 2020, en la Rama Estudiantil IEEE de la UTN-FRT se renovaron las autoridades. Agus y
+      Germán dieron otro paso más en su carrera del voluntariado y Mauricio Sánchez se sumó también.
+      Agus era presidente, Germán vicepresidente y Mauri tesorero. Se sumaron aquel año también
+      Chelo, Iván y Esteban para colaborar en la organización de las actividades.
+    </p>
+
+    <p>
+      2020 estuvo marcado por la cuarentena por la pandemia del COVID-19, por lo que todos los
+      eventos organizados eran virtuales, nada presencial. El evento estrella eran las Lightning
+      Talks, jornada de charlas rápidas sobre ingeniería de software.
+    </p>
+
+    <p>
+      El IEEE tenía muchas limitaciones que nos impedían crecer por el rumbo que queríamos y
+      dedicarnos 100% a la programación y crear la comunidad que visionabamos, por lo cual,
+      decidimos dar un paso al costado y crear nuestro propio espacio de voluntariado. Es así como
+      nace programaConNosotros (PCN para los amigos).
+    </p>
+
+    <Heading3>Cursos de Git & GitHub con la cátedra de AED</Heading3>
+
+    <p>
+      La cátedra de Algoritmos y Estructuras de Datos (AED) de la UTN-FRT le dio lugar a la
+      comunidad para dar algunos talleres de Git y GitHub, con los cuales se sumó mucha gente a la
+      comunidad, entre los cuales estaban Tobias Paz Posse, Jeremias Ivanoff y Lucas Pérez. Tobi,
+      Jere (alias Lunai) y Lucas le pusieron mucha energía y buena onda a la comunidad, y han
+      crecido mucho desde que entraron.
+    </p>
+
+    <Heading3>Lightning Talks</Heading3>
+
+    <p>
+      Organizamos varias jornadas de Lightning Talks, donde muchos miembros de la comunidad
+      comparten su conocimiento y experiencias. Al principio eran todas virtuales por Discord, y
+      cuando se terminó la cuarentena y se pudo volver a la vida de antes, organizamos varias
+      ediciones de Lightning Talks presenciales.
+    </p>
+
+    <Heading3>PCN & Global Learning</Heading3>
+
+    <p>
+      En 2023, colaboramos con Global Learning dando charlas sobre arquitectura de software e
+      ingeniería de software en la última clase de sus cursos.
+    </p>
+
+    <Heading3>Empezamos a desarrollar el website</Heading3>
+
+    <p>
+      En 2024, Agus empezó el proyecto del website de PCN. Al principio iba a ser solo un website
+      institucional, pero después se transformó en una red social más compleja, en la que los
+      miembros de la comunidad pueden crear un usuario para interactuar en el sitio.
+    </p>
+
+    <p>
+      El proyecto es open-source y cualquier persona de la comunidad puede participar en el
+      desarrollo y testing de la plataforma. Los miembros de la comunidad que más aportaron en el
+      inicio de la implementación de este proyecto fueron Germán Navarro, Mauricio Chaile, Matías
+      Gutierrez, Vicky Grillo y Emiliano Grillo. El equipo fue y es actualmente liderado por Agus.
     </p>
   </div>
 );
