2cb3f6842cbbc4833e9eb1d95c4b95b39a6e5115
agustin-sanc <js.agustin.sz@gmail.com>
2025-06-05 21:25:19 +0000

    fix: youtube link

--- src/components/ui/app-sidebar.tsx
+++ src/components/ui/app-sidebar.tsx
@@ -135,7 +135,7 @@ const data = {
     // },
     {
       name: 'YouTube',
-      url: 'https://www.youtube.com/@programaconnosotros',
+      url: 'https://www.youtube.com/@programaconnosotros2689/videos',
       icon: Youtube,
     },
     {
