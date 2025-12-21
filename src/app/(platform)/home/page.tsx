c002223c65ba8b9b96348d91743efc84d5d3a3d1
agustin-sanc <js.agustin.sz@gmail.com>
2025-12-21 00:35:47 +0000

    feat: create Home component with session management and breadcrumb navigation

---
+++ src/app/(platform)/home/page.tsx
@@ -0,0 +1,52 @@
+import prisma from '@/lib/prisma';
+import { Session, User } from '@prisma/client';
+import { cookies } from 'next/headers';
+import HomeClientSide from '@/app/(platform)/home-client-side';
+import {
+  BreadcrumbLink,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import { SidebarTrigger } from '@/components/ui/sidebar';
+
+const Home = async () => {
+  const sessionId = cookies().get('sessionId')?.value;
+
+  let session: (Session & { user: User }) | null = null;
+
+  if (sessionId) {
+    session = await prisma.session.findUnique({
+      where: {
+        id: sessionId,
+      },
+      include: {
+        user: true,
+      },
+    });
+  }
+
+  return (
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/home">Inicio</BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <HomeClientSide session={session} />
+    </>
+  );
+};
+
+export default Home;
