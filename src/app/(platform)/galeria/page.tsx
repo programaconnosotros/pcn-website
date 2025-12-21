87b18214aed3ad2a972adc5aacab5b7f30466b63
agustin-sanc <js.agustin.sz@gmail.com>
2025-12-21 09:35:18 +0000

    feat: enhance PhotoGallery layout with breadcrumb navigation and sidebar trigger for improved user experience

--- src/app/(platform)/galeria/page.tsx
+++ src/app/(platform)/galeria/page.tsx
@@ -4,6 +4,16 @@ import { Gallery } from '@/components/photo-gallery/gallery';
 import { Heading2 } from '@/components/ui/heading-2';
 import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/components/ui/breadcrumb';
+import { Separator } from '@/components/ui/separator';
+import { SidebarTrigger } from '@/components/ui/sidebar';
 
 export default function PhotoGallery() {
   const searchParams = useSearchParams();
@@ -20,16 +30,35 @@ export default function PhotoGallery() {
   }, [photoId]);
 
   return (
-    <div className="mt-4 md:px-20">
-      <div className="z-10 bg-background/95 pb-3 pt-1 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <div className="flex w-full flex-row justify-between">
-          <Heading2 className="m-0">Galería</Heading2>
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger />
+          <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/home">Inicio</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Galería</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="mt-4">
+          <div className="mb-4 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="flex w-full flex-row justify-between">
+              <Heading2 className="m-0">Galería</Heading2>
+            </div>
+          </div>
+
+          <Gallery initialPhotoId={initialPhotoId} />
         </div>
       </div>
-
-      <main className="mx-auto my-4">
-        <Gallery initialPhotoId={initialPhotoId} />
-      </main>
-    </div>
+    </>
   );
 }
