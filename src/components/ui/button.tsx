b91c6ef15856cd32a53f6495c351b9712107c697
agustin-sanc <js.agustin.sz@gmail.com>
2025-12-21 00:46:16 +0000

    fix: update default button styles for improved visual consistency in UI

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary dark:bg-pcnGreen text-primary-foreground hover:bg-primary/90',
+        default: 'bg-secondary dark:bg-secondary text-secondary-foreground hover:bg-secondary/80 dark:hover:bg-secondary/70',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
