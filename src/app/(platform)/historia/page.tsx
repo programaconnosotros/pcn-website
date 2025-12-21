5ebf30da25f72d72fe538b1556613e3fb105c777
agustin-sanc <js.agustin.sz@gmail.com>
2025-12-21 20:55:43 +0000

    style: increase bottom margin of background container in historia page for improved spacing and layout

--- src/app/(platform)/historia/page.tsx
+++ src/app/(platform)/historia/page.tsx
@@ -38,7 +38,7 @@ const PCNStory = () => (
     </header>
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
       <div className="mt-4">
-        <div className="mb-4 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="mb-12 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
           <div className="flex flex-col items-center gap-4">
             <img src="/logo.webp" alt="programaConNosotros" className="mb-4 w-16 md:w-24" />
             <Heading2 className="m-0 text-center">Historia de programaConNosotros</Heading2>
