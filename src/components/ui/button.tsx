24ec0ed80dd0bd6a44cfb3800272ad3ce65b2478
Agustin Sanchez <js.agustin.sz@gmail.com>
2025-12-26 18:44:11 +0000

    feat: update button variant styles in button component for improved visual consistency and user experience

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-secondary dark:bg-secondary text-secondary-foreground hover:bg-secondary/80 dark:hover:bg-secondary/70',
+          'bg-pcnPurple text-white hover:bg-pcnPurple/90 dark:bg-pcnGreen dark:text-black dark:hover:bg-pcnGreen/90',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
