f6a690dfd9c57b79bb1e03a2740ead7e9842c935
Agustin Sanchez <js.agustin.sz@gmail.com>
2025-12-29 01:05:50 +0000

    fix: update breadcrumb link to point to the home page for improved navigation

--- src/app/(platform)/eventos/[id]/page.tsx
+++ src/app/(platform)/eventos/[id]/page.tsx
@@ -229,7 +229,7 @@ const EventDetailPage: React.FC<{ params: { id: string } }> = async ({ params })
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="/home">Inicio</BreadcrumbLink>
+                <BreadcrumbLink href="/">Inicio</BreadcrumbLink>
               </BreadcrumbItem>
               <BreadcrumbSeparator className="hidden md:block" />
               <BreadcrumbItem className="hidden md:block">
