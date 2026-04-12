1f2ad7a01ae8b53b5231e4d4d3d9e553b60e959a
agustin-sanc <js.agustin.sz@gmail.com>
2026-04-12 07:24:04 -0300

    refactor(perfil): remove hover zoom effect from profile card

--- src/app/(platform)/perfil/[id]/page.tsx
+++ src/app/(platform)/perfil/[id]/page.tsx
@@ -206,7 +206,7 @@ export default async function ProfilePage({ params }: ProfilePageProps) {
           {/* Columna izquierda: Información del usuario (fija en pantallas grandes) */}
           <div className="lg:col-span-1">
             <div className="lg:sticky lg:top-4">
-              <Card className="border-2 border-transparent bg-gradient-to-br from-white to-gray-50 transition-all duration-300 hover:scale-[1.02] hover:border-pcnPurple hover:shadow-xl dark:border-neutral-800 dark:from-neutral-900 dark:to-neutral-800 dark:hover:border-pcnGreen dark:hover:shadow-pcnGreen/20">
+              <Card className="border-2 border-transparent bg-gradient-to-br from-white to-gray-50 transition-all duration-300 hover:border-pcnPurple hover:shadow-xl dark:border-neutral-800 dark:from-neutral-900 dark:to-neutral-800 dark:hover:border-pcnGreen dark:hover:shadow-pcnGreen/20">
                 <CardHeader className="flex flex-col items-center gap-4 pb-6">
                   <Avatar className="h-32 w-32">
                     <AvatarImage src={user.image ?? undefined} alt={user.name ?? 'Usuario'} />
