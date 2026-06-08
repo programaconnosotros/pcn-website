80327f27464877776d415f10137e71f978352ba6
agustin-sanc <js.agustin.sz@gmail.com>
2026-06-08 17:04:41 -0300

    Revert "chore(migrations): recover missing migration files from DB history"
    This reverts commit 9c658cb1220d15d3d50b199f091152680f5bb039.

--- prisma/migrations/20260606000000_add_flyer_images_carousel/migration.sql
+++
@@ -1,8 +0,0 @@
--- Add flyerImages array column with empty default
-ALTER TABLE "Event" ADD COLUMN "flyerImages" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[];
-
--- Backfill: copy existing flyerSrc into flyerImages as a single-element array
-UPDATE "Event" SET "flyerImages" = ARRAY["flyerSrc"] WHERE "flyerSrc" IS NOT NULL AND "flyerSrc" <> '';
-
--- Drop the old flyerSrc column
-ALTER TABLE "Event" DROP COLUMN "flyerSrc";
